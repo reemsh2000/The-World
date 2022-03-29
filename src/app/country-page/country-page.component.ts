7ad1dc4c99c6505b60dfafa94e79f087a2df11c3
reemsh2000 <sh951652@gmail.com>
2022-03-29 21:55:08 +0300

    country page

--- src/app/country-page/country-page.component.ts
+++ src/app/country-page/country-page.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CountriesDataService } from '../countries-data.service';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { CountriesDataService } from '../countries-data.service'
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.css'],
 })
 export class CountryPageComponent implements OnInit {
-  country: any;
+  country: any
+  borderCountries: any
+  loading=true;
+
   constructor(
     private route: ActivatedRoute,
-    private countriesService: CountriesDataService
+    private countriesService: CountriesDataService,
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
-      let countryName:any=params.get('country')
-      this.countriesService.getCountryInfo(countryName).subscribe((data)=>{
-        console.log(data)
-      }
-
-      )
-    });
+      let countryName: any = params.get('country')
+      this.countriesService
+        .getCountryInfo(countryName)
+        .subscribe((data: any) => {
+          this.country = data[0]
+          this.loading=false;
+          this.borderCountries = this.country.borders
+          console.log(this.country)
+        })
+    })
   }
-
 }
