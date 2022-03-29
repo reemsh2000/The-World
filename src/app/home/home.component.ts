ecf20775bd670b7efe733010f4054e66cab26fa6
reemsh2000 <sh951652@gmail.com>
2022-03-29 15:06:48 +0300

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CountriesDataService } from '../countries-data.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  countries: any
+  loading = true
+  selectedRegion: any
+  regions = [
+    { name: 'Europe' },
+    { name: 'Americas' },
+    { name: 'Oceania' },
+    { name: 'Africa' },
+    { name: 'Asia' },
+  ]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private countryService: CountriesDataService) {}
+  ngOnInit() {
+    this.getCountriesInfo()
+    console.log(this.selectedRegion)
+  }
+  getCountriesInfo() {
+    this.countryService.getAllCountries().subscribe((data) => {
+      this.countries = data
+      this.loading = false
+    })
   }
 
+  filterByRegion(region: string) {
+    this.countryService.getRegionCountries(region).subscribe((data) => {
+      this.countries = data
+      this.loading = false
+    })
+  }
+  searchAboutCountry(country: string) {
+    this.countryService.getCountryInfo(country).subscribe((data) => {
+      this.countries = data
+      this.loading = false
+    })
+
+  }
+ 
 }
