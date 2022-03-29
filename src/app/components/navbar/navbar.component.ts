5dadc56c3bc01bf51b72484275449f14a5c89236
reemsh2000 <sh951652@gmail.com>
2022-03-29 22:40:04 +0300

    dark mode '

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  checked = false;
+  inDarkMode = false;
 
   countryName: any;
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document) {}
   ngOnInit(): void {}
 
-  setDarkMode(e: any) {
-    console.log(e);
+  setDarkMode(checked:boolean) {
+    this.document.body.classList.toggle('dark');
   }
 }
