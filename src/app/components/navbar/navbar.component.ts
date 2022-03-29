a37bbdace817615d0a2d5613b005fa4aa4c55655
reemsh2000 <sh951652@gmail.com>
2022-03-29 15:06:10 +0300

    navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  checked = false;
 
-  constructor() { }
+  countryName: any;
+  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  setDarkMode(e: any) {
+    console.log(e);
   }
-
 }
