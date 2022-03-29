5c16422d8676e006c2242777d83949e042efb8d7
reemsh2000 <sh951652@gmail.com>
2022-03-29 22:46:15 +0300

    remove style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { CountriesDataService } from './countries-data.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  templateUrl: './app.component.html'})
 export class AppComponent {
   title = 'countries';
   countryName: any
